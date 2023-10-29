b3e23618dc176e3de057049946c2840e6f190ef2
adanfj <ajffj4@gmail.com>
2023-10-29 21:00:42 +0100

    V9

--- src/components/AppLayout.svelte
+++ src/components/AppLayout.svelte
@@ -8,16 +8,17 @@
         faTwitter,
         faYoutube,
     } from "@fortawesome/free-brands-svg-icons";
-    import { faBars, faVideo } from "@fortawesome/free-solid-svg-icons";
-    import Icon from "@iconify/svelte";
+    import { faBars } from "@fortawesome/free-solid-svg-icons";
     import twotoneWebStories from "@iconify/icons-ic/twotone-web-stories";
+    import Icon from "@iconify/svelte";
     import Fa from "svelte-fa";
     import "../app.css";
-    // import { LightSwitch } from "@skeletonlabs/skeleton";
+// import { LightSwitch } from "@skeletonlabs/skeleton";
     import { fly } from "svelte/transition";
     export let socialMedia: { [k: string]: URL } = {};
     export let links: { [k: string]: string } = {};
     export let currentPath: string;
+    export let cv: boolean = false;
     $: isCurrentPath = (link: string) =>
         (link == "/" && currentPath == link) ||
         (currentPath.startsWith(link) && link != "/");
@@ -56,89 +57,93 @@
     };
 </script>
 
-<nav
-    use:clickOutside={{ enabled: navOpen, cb: () => (navOpen = false) }}
-    class="px-8 py-8 lg:px-24 justify-between items-center"
->
-    <a
-        href="/"
-        class="flex space-x-4 items-center hover:variant-soft-primary dark:hover:bg-transparent hover:bg-transparent"
-    >
-        <!-- <Fa icon={faVideo} size="2x" /> -->
-        <Icon icon={twotoneWebStories} width="3rem" />
-        <h2 class="">Adan</h2>
-    </a>
-    <div
-        class="variant-filled-surface rounded-container-token p-4 space-x-4 shadow-md shadow-surface-400 dark:shadow-surface-600 hidden lg:block"
-    >
-        {#each Object.entries(links) as [link, title]}
-            <a
-                href={link}
-                class="text-xl rounded-container-token py-2 px-4 font-bold"
-                class:variant-filled={isCurrentPath(link)}>{title}</a
-            >
-        {/each}
-    </div>
-    <button
-        class="btn flex justify-center items-center lg:hidden bg-surface-100-800-token rounded-container-token w-8 aspect-square"
-        title="Abrir menú de navegación"
-        on:click={() => (navOpen = !navOpen)}><Fa icon={faBars} /></button
-    >
-    <div class="hidden lg:flex gap-8 items-center">
-        <div class="flex space-x-4 items-center">
-            {#each Object.entries(socialMedia) as [name, link]}
-                <a
-                    href={link.href}
-                    target="_blank"
-                    aria-label="Accede a mi link de {name}"
-                    ><Fa
-                        icon={linkIcons[name]}
-                        size="2x"
-                        class="text-primary-500-400-token"
-                    /></a
-                >
-            {/each}
-        </div>
-        <!-- <LightSwitch /> -->
-    </div>
-    {#if navOpen}
-        <div
-            transition:fly={{ x: "400px" }}
-            class="fixed z-20 top-0 right-0 bg-surface-600-300-token text-surface-50-900-token flex flex-col lg:hidden max-w-[20rem] w-full shadow-md"
-        >
-            <div
-                class="p-4 border-b border-b-surface-500 dark:border-b-surface-400 flex flex-col space-y-8"
-            >
-                {#each Object.entries(links) as [link, title]}
-                    <a
-                        href={link}
-                        class="text-2xl"
-                        class:font-bold={isCurrentPath(link)}>{title}</a
-                    >
-                {/each}
-            </div>
-            <div class="py-8 px-4 flex justify-between items-center">
-                <div class="flex space-x-4 items-center">
-                    {#each Object.entries(socialMedia) as [name, link]}
-                        <a href={link.href} target="_blank"
-                            ><Fa
-                                icon={linkIcons[name]}
-                                size="2x"
-                                class="text-primary-500-400-token"
-                            /></a
-                        >
-                    {/each}
-                </div>
-                <!-- <LightSwitch /> -->
-            </div>
-        </div>
-    {/if}
-</nav>
+{#if !cv}
+     <nav
+         use:clickOutside={{ enabled: navOpen, cb: () => (navOpen = false) }}
+         class="px-8 py-8 lg:px-24 justify-between items-center"
+     >
+         <a
+             href="/"
+             class="flex space-x-4 items-center hover:variant-soft-primary dark:hover:bg-transparent hover:bg-transparent"
+         >
+             <!-- <Fa icon={faVideo} size="2x" /> -->
+             <Icon icon={twotoneWebStories} width="3rem" />
+             <h2 class="">Adan</h2>
+         </a>
+         <div
+             class="variant-filled-surface rounded-container-token p-4 space-x-4 shadow-md shadow-surface-400 dark:shadow-surface-600 hidden lg:block"
+         >
+             {#each Object.entries(links) as [link, title]}
+                 <a
+                     href={link}
+                     class="text-xl rounded-container-token py-2 px-4 font-bold"
+                     class:variant-filled={isCurrentPath(link)}>{title}</a
+                 >
+             {/each}
+         </div>
+         <button
+             class="btn flex justify-center items-center lg:hidden bg-surface-100-800-token rounded-container-token w-8 aspect-square"
+             title="Abrir menú de navegación"
+             on:click={() => (navOpen = !navOpen)}><Fa icon={faBars} /></button
+         >
+         <div class="hidden lg:flex gap-8 items-center">
+             <div class="flex space-x-4 items-center">
+                 {#each Object.entries(socialMedia) as [name, link]}
+                     <a
+                         href={link.href}
+                         target="_blank"
+                         aria-label="Accede a mi link de {name}"
+                         ><Fa
+                             icon={linkIcons[name]}
+                             size="2x"
+                             class="text-primary-500-400-token"
+                         /></a
+                     >
+                 {/each}
+             </div>
+             <!-- <LightSwitch /> -->
+         </div>
+         {#if navOpen}
+             <div
+                 transition:fly={{ x: "400px" }}
+                 class="fixed z-20 top-0 right-0 bg-surface-600-300-token text-surface-50-900-token flex flex-col lg:hidden max-w-[20rem] w-full shadow-md"
+             >
+                 <div
+                     class="p-4 border-b border-b-surface-500 dark:border-b-surface-400 flex flex-col space-y-8"
+                 >
+                     {#each Object.entries(links) as [link, title]}
+                         <a
+                             href={link}
+                             class="text-2xl"
+                             class:font-bold={isCurrentPath(link)}>{title}</a
+                         >
+                     {/each}
+                 </div>
+                 <div class="py-8 px-4 flex justify-between items-center">
+                     <div class="flex space-x-4 items-center">
+                         {#each Object.entries(socialMedia) as [name, link]}
+                             <a href={link.href} target="_blank"
+                                 ><Fa
+                                     icon={linkIcons[name]}
+                                     size="2x"
+                                     class="text-primary-500-400-token"
+                                 /></a
+                             >
+                         {/each}
+                     </div>
+                     <!-- <LightSwitch /> -->
+                 </div>
+             </div>
+         {/if}
+     </nav>
+{/if}
+
 
 <div class="mt-16 px-8 lg:px-24">
     <slot />
 </div>
 
+{#if !cv}
 <footer>
     <p class="font-bold">
         &copy;{new Date().getFullYear()} Adan. Todos los derechos reservados.
@@ -158,3 +163,4 @@
         {/each}
     </div>
 </footer>
+{/if}
---
+++ src/components/CV.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+    import { fly } from "svelte/transition";
+    import Group from "./CV/Group.svelte";
+
+    export let name;
+    export let profession;
+    export let studies: { [k: string]: string } = {};
+    export let socialMedia: { [k: string]: string } = {};
+    export let experience: { [k: string]: string } = {};
+    export let languages: { [k: string]: string } = {};
+    const years = (date: Date) => {
+        let years = 0,
+            refDate = new Date(
+                date.getFullYear() + 1,
+                date.getMonth(),
+                date.getDate()
+            ),
+            now = new Date();
+        while (refDate.getTime() < now.getTime()) {
+            refDate = new Date(
+                refDate.getFullYear() + 1,
+                refDate.getMonth(),
+                refDate.getDate()
+            );
+            console.log(refDate);
+            years++;
+        }
+        return years;
+    };
+    let currentYears = years(new Date("July 8 1998"));
+</script>
+
+<div
+    id="cv"
+    class="grid w-[64rem] py-4 slide-in-fwd-center"
+    style="grid-template-columns: repeat(14, minmax(0, 1fr));"
+>
+    <div
+        class="flex flex-col items-center bg-primary-300 dark:bg-primary-900 col-span-5 py-12 gap-24 h-full"
+    >
+        <div class="w-[70%] space-y-8">
+            <div class="rounded-full overflow-hidden slide-in-fwd-center" style="animation-delay: 0.5s">
+                <img class="slide-in-fwd-center w-full" src="/portrait.jpg" alt="" />
+            </div>
+            <div class="slide-in-left" style="animation-delay: 0.6s;">
+                <Group title="Perfil Profesional">
+                    Soy un chico de {currentYears} años muy activo, energético y flexible
+                    interesado en el bienestar de las personas de todas las edades y
+                    ámbitos. Manejo bien el estrés y ciertas situaciones comprometidas
+                    sin perder la paciencia, además de que siempre busco soluciones en
+                    todo momento de adversidad.
+                </Group>
+            </div>
+        </div>
+        <div class="w-[80%] slide-in-left" style="animation-delay: 0.8s;">
+            <Group title="Contacta conmigo">
+                <ul>
+                    {#each Object.entries(socialMedia) as [k, value]}
+                        <li>{k}: <span class="font-bol">{value}</span></li>
+                    {/each}
+                </ul>
+            </Group>
+        </div>
+    </div>
+    <div class="col-span-9 flex flex-col">
+        <div
+            class="aspect-[16/7.5] bg-primary-600-300-token text-primary-200-700-token flex flex-col items-center text-center pt-12 pb-16"
+        >
+            <h1>{name}</h1>
+            <p>{profession}</p>
+        </div>
+        <div class="bg-surface-50-900-token w-full h-full p-16 space-y-8">
+            <div class="slide-in-right" style="animation-delay: 1s;">
+                <Group title="Estudios académicos">
+                    <ul>
+                        {#each Object.entries(studies) as [k, value]}
+                            <li>{k} - {value}</li>
+                        {/each}
+                    </ul>
+                </Group>
+            </div>
+            <div class="slide-in-right" style="animation-delay: 1.2s;">
+                <Group title="Experiencia" >
+                    <!-- <ul>
+                        {#each Object.entries(experience) as [k, value]}
+                            <li>{k} - {value}</li>
+                        {/each}
+                    </ul> -->
+                    Ayudé a programar diversos proyectos de programación orientados a la creación de páginas web dedicadas a márketing y educación escolar.
+                </Group>
+            </div>
+                <div class="slide-in-right" style="animation-delay: 1.4s;">
+
+                <Group title="Idiomas" >
+                    <ul>
+                        {#each Object.entries(languages) as [k, value]}
+                            <li>{k}: {value}</li>
+                        {/each}
+                    </ul>
+                </Group>
+            </div>
+        </div>
+    </div>
+</div>
